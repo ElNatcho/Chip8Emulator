3c57b042ca5565fea50f656f2217c9e84f928291
ElNatcho <gebhardsven@gmail.com>
2017-04-27 19:59:46 +0200

    Chip8Compiler: >CCompiler: ->_compileInst: -->Methode hinzugefügt und teilweise definiert ->_regex, ... als Member Variable hinzugefügt ->Grundlegende Methoden für Übersetzung erzeugt (tCLS, ...)

--- Chip8Compiler/CCompiler.cpp
+++ Chip8Compiler/CCompiler.cpp
@@ -3,7 +3,10 @@
 // -- Konstruktor --
 CCompiler::CCompiler() {
 	// Alloc Mem
+	_regex = new std::regex();
+	_match = new std::smatch();
 	_jmpAddr = new std::map<std::string, short>();
+	_opcodes = new std::vector<short>();
 	_srcLoader = new CSrcLoader();
 	_sourceCode = new std::vector<std::string>();
 }
@@ -22,31 +25,47 @@ void CCompiler::loadSrc(std::string path) {
 //
 void CCompiler::compile() {
 	getJmpAddr();
+	for (int i = 0; i < _sourceCode->size(); i++) {
+		_compileInstr(_sourceCode->at(i));
+	}
 }
 
 // -- getJmpAddr --
 // Methode sucht nach Jump-Adressen und speichert diese in der map bzw. löscht sie aus dem Code
 //
 void CCompiler::getJmpAddr() {
-	std::regex  reg(":[\\w\\d]+((\t)|(\\s))");
-	std::smatch match;
+	*_regex = ":[\\w\\d]+((\t)|(\\s))";
 	for (int i = 0; i < _sourceCode->size(); i++) { // Durch den kompletten Sourc-Code iterieren
-		if (std::regex_search(_sourceCode->at(i), match, reg)) {
+		if (std::regex_search(_sourceCode->at(i), *_match, *_regex)) {
 			_jmpAddr->insert(std::make_pair(
-				match.str().substr(1, match.str().size() - 2),
+				_match->str().substr(1, _match->str().size() - 2),
 				i * 2 + 0x200
 			));
-			_sourceCode->at(i) = match.suffix().str();
-			match.empty();
+			_sourceCode->at(i) = _match->suffix().str();
+			_match->empty();
 		}
 	}
+}
+
+// -- compileInstr --
+// Methode compilet eine Instruktion
+// @path instr: Instruktion die compiliert werden soll
+//
+void CCompiler::_compileInstr(std::string instr) {
+	*_regex = "((\\s*)|(\t*))[\\w]+(0?)((\\s*)|(\t*))";
+	std::regex_search(instr, *_match, *_regex);
+	std::cout << _match->str() << std::endl << std::endl;
 }
 
 // -- Destruktor --
 CCompiler::~CCompiler() {
 	// Free Mem
+	SAFE_DELETE(_regex);
+	SAFE_DELETE(_match);
 	_jmpAddr->clear();
 	SAFE_DELETE(_jmpAddr);
+	_opcodes->clear();
+	SAFE_DELETE(_opcodes);
 	SAFE_DELETE(_srcLoader);
 	_sourceCode->clear();
 	SAFE_DELETE(_sourceCode);
--- Chip8Compiler/CCompiler.hpp
+++ Chip8Compiler/CCompiler.hpp
@@ -59,14 +59,48 @@ public:
 private:
 
 	// -- Member Vars --
+	CSrcLoader *_srcLoader;
 	std::vector<std::string> *_sourceCode;
 
 	std::map<std::string, short> *_jmpAddr; // Map speichert die Adressen der Jump-Adressen
 
-	CSrcLoader *_srcLoader;
+	std::vector<short> *_opcodes;
+
+	std::regex  *_regex;
+	std::smatch *_match;
 
 	// -- Member Methods --
 	void getJmpAddr();
+	void _compileInstr(std::string instr);
+
+	// - Translate Methods -
+	short tCLS   (std::string args);
+	short tRET   (std::string args);
+	short tJMP   (std::string args);
+	short tIE    (std::string args);
+	short tINE   (std::string args);
+	short tMOV   (std::string args);
+	short tADD   (std::string args);
+	short tOR    (std::string args);
+	short tADD   (std::string args);
+	short tXOR   (std::string args);
+	short tSUB   (std::string args);
+	short tRSH   (std::string args);
+	short tSUBC  (std::string args);
+	short tLSH   (std::string args);
+	short tJMP0  (std::string args);
+	short tRND   (std::string args);
+	short tDRW   (std::string args);
+	short tIKPR  (std::string args);
+	short tIKNPR (std::string args);
+	short tGDT   (std::string args);
+	short tWFKPR (std::string args);
+	short tSDT   (std::string args);
+	short tSST   (std::string args);
+	short tSISP  (std::string args);
+	short tBCD   (std::string args);
+	short tRDMP  (std::string args);
+	short tRLOD  (std::string args);
 
 };
 
