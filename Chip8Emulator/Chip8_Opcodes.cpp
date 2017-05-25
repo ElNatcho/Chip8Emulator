079ed6978efee28a14dc735c1c8b31b463400b9e
ElNatcho <gebhardsven@gmail.com>
2017-05-25 12:04:19 +0200

    Chip8Emulator: >BCD-Bug gefixt >main: ->Delay-Timer hinzugefügt

--- Chip8Emulator/Chip8_Opcodes.cpp
+++ Chip8Emulator/Chip8_Opcodes.cpp
@@ -116,7 +116,6 @@ void Chip8::compr_0x5XY0() {
 //
 void Chip8::setr_0x6XNN() {
 	_reg_v->at((*_opcode & 0x0F00) >> 8) = *_opcode & 0x00FF;
-	//*_reg_pc += 2;
 }
 
 // -- add_0x7XNN --
@@ -124,7 +123,6 @@ void Chip8::setr_0x6XNN() {
 //
 void Chip8::add_0x7XNN() {
 	_reg_v->at((*_opcode & 0x0F00) >> 8) += *_opcode & 0x00FF;
-	//*_reg_pc += 2;
 }
 
 // -- setr_0x8XY0 --
@@ -132,7 +130,6 @@ void Chip8::add_0x7XNN() {
 //
 void Chip8::setr_0x8XY0() {
 	_reg_v->at((*_opcode & 0x0F00) >> 8) = _reg_v->at((*_opcode & 0x00F0) >> 4);
-	//*_reg_pc += 2;
 }
 
 // -- bitOR_0x8XY1 --
@@ -141,7 +138,6 @@ void Chip8::setr_0x8XY0() {
 void Chip8::bitOR_0x8XY1() {
 	_reg_v->at((*_opcode & 0x0F00) >> 8) |= _reg_v->at((*_opcode & 0x00F0) >> 4);
 	_reg_v->at(0xF) = 0;
-	//*_reg_pc += 2;
 }
 
 // -- bitAND_0x8XY2 --
@@ -150,7 +146,6 @@ void Chip8::bitOR_0x8XY1() {
 void Chip8::bitAND_0x8XY2() {
 	_reg_v->at((*_opcode & 0x0F00) >> 8) &= _reg_v->at((*_opcode & 0x00F0) >> 4);
 	_reg_v->at(0xF) = 0;
-	//*_reg_pc += 2;
 }
 
 // -- bitXOR_0x8XY3 --
@@ -159,7 +154,6 @@ void Chip8::bitAND_0x8XY2() {
 void Chip8::bitXOR_0x8XY3() {
 	_reg_v->at((*_opcode & 0x0F00) >> 8) ^= _reg_v->at((*_opcode & 0x00F0) >> 4);
 	_reg_v->at(0xF) = 0;
-	//*_reg_pc += 2;
 }
 
 // -- addr_0x8XY4 --
@@ -172,7 +166,6 @@ void Chip8::addr_0x8XY4() {
 	else
 		_reg_v->at(0xF) = 0;
 	_reg_v->at((*_opcode & 0x0F00) >> 8) = e & 0x0000FFFF;
-	//*_reg_pc += 2;
 }
 
 // -- subr_0x8XY5 --
@@ -185,7 +178,6 @@ void Chip8::subr_0x8XY5() {
 	else
 		_reg_v->at(0xF) = 0;
 	_reg_v->at((*_opcode & 0x0F00) >> 8) = e & 0x0000FFFF;
-	//*_reg_pc += 2;
 }
 
 // -- bitShR_0x8XY6 --
@@ -194,7 +186,6 @@ void Chip8::subr_0x8XY5() {
 void Chip8::bitShR_0x8XY6() {
 	_reg_v->at(0xF) = _reg_v->at((*_opcode & 0x0F00) >> 8) & 0x01;
 	_reg_v->at((*_opcode & 0x0F00) >> 8) >>= 1;
-	//*_reg_pc += 2;
 }
 
 // -- subr_0x8XY7 -- 
@@ -207,7 +198,6 @@ void Chip8::subr_0x8XY7() {
 	else
 		_reg_v->at(0xF) = 0;
 	_reg_v->at((*_opcode & 0x0F00) >> 8) = e & 0x0000FFFF;
-	//*_reg_pc += 2;
 }
 
 // -- bitShL_0x8XYE --
@@ -216,7 +206,6 @@ void Chip8::subr_0x8XY7() {
 void Chip8::bitShL_0x8XYE() {
 	_reg_v->at(0xF) = (_reg_v->at((*_opcode & 0x0F00) >> 8) & 0xF000) >> 3;
 	_reg_v->at((*_opcode & 0x0F00) >> 8) <<= 1;
-	//*_reg_pc += 2;
 }
 
 // -- compnr_0x9XY0 --
@@ -234,7 +223,6 @@ void Chip8::compnr_0x9XY0() {
 //
 void Chip8::setI_0xANNN() {
 	*_reg_i = *_opcode & 0x0FFF;
-	//*_reg_pc += 2;
 }
 
 // -- jumpR_0xBNNN --
@@ -249,7 +237,6 @@ void Chip8::jumpR_0xBNNN() {
 //
 void Chip8::setR_rand_0xCXNN() {
 	_reg_v->at((*_opcode & 0x0F00) >> 8) = (rand() % 255) & (*_opcode & 0x00FF);
-	//*_reg_pc += 2;
 }
 
 // -- drawSpr_0xDXYN --
@@ -273,7 +260,6 @@ void Chip8::drawSpr_0xDXYN() {
 		}
 	}
 	*drawFlag = true;
-	//*_reg_pc += 2;
 }
 
 // -- chkKeyR_0xEX9E --
@@ -301,7 +287,6 @@ void Chip8::chkNKeyR_0xEXA1() {
 //
 void Chip8::setRTimer_0xFX07() {
 	_reg_v->at((*_opcode & 0x0F00) >> 8) = *_delay_timer;
-	//*_reg_pc += 2;
 }
 
 // -- waitFKey_0xFX0A --
@@ -312,8 +297,6 @@ void Chip8::waitFKey_0xFX0A() {
 		handleKeys();
 	} while (!*_keyPushedFlag);
 	_reg_v->at((*_opcode & 0x0F00) >> 8) = *_lastKeyPushed;
-	
-	//*_reg_pc += 2;
 }
 
 // -- setDTimer_0xFX15 --
@@ -321,7 +304,6 @@ void Chip8::waitFKey_0xFX0A() {
 //
 void Chip8::setDTimer_0xFX15() {
 	*_delay_timer = _reg_v->at((*_opcode & 0x0F00) >> 8);
-	//*_reg_pc += 2;
 }
 
 // -- setSTimer_0xFX18 --
@@ -329,7 +311,6 @@ void Chip8::setDTimer_0xFX15() {
 //
 void Chip8::setSTimer_0xFX18() {
 	*_sound_timer = _reg_v->at((*_opcode & 0x0F00) >> 8);
-	//*_reg_pc += 2;
 }
 
 // -- addI_0xFX1E --
@@ -337,7 +318,6 @@ void Chip8::setSTimer_0xFX18() {
 //
 void Chip8::addI_0xFX1E() {
 	*_reg_i += _reg_v->at((*_opcode & 0x0F00) >> 8);
-	//*_reg_pc += 2;
 }
 
 // -- setItoSpr_0xFX29 -- 
@@ -345,7 +325,6 @@ void Chip8::addI_0xFX1E() {
 //
 void Chip8::setItoSpr_0xFX29() {
 	*_reg_i = _reg_v->at((*_opcode & 0x0F00) >> 8) * 40;
-	//*_reg_pc += 2;
 }
 
 // -- set_BCD_0xFX33 --
@@ -353,19 +332,15 @@ void Chip8::setItoSpr_0xFX29() {
 void Chip8::set_BCD_0xFX33() {
 	unsigned short v = _reg_v->at((*_opcode & 0x0F00) >> 8);
 	unsigned short h = v / 100;
-	v -= 100 * h;
-	unsigned short z = v / 10;
-	v -= 10 * z;
-	unsigned short e = v / 1;
-	v -= 1 * e;
-	if (e != 0)
-		throw std::exception("BCD_ERR");
+	unsigned short z = (v - (h * 100)) / 10;
+	unsigned short e = (v - (h * 100 + z * 10));
+
+	/*if (e != 0)
+		throw std::exception("BCD_ERR");*/
 
 	_memory->at(*_reg_i) = h;
 	_memory->at(*_reg_i + 1) = z;
 	_memory->at(*_reg_i + 2) = e;
-
-	//*_reg_pc += 2;
 }
 
 // -- reg_dump_0xFX55 --
@@ -375,7 +350,6 @@ void Chip8::reg_dump_0xFX55() {
 	for (int i = 0; i < ((*_opcode & 0x0F00) >> 8); i++) {
 		_memory->at(*_reg_i + i) = _reg_v->at(i);
 	}
-	//*_reg_pc += 2;
 }
 
 // -- reg_load_0xFX65 --
@@ -385,5 +359,4 @@ void Chip8::reg_load_0xFX65() {
 	for (int i = 0; i < ((*_opcode & 0x0F00) >> 8); i++) {
 		_reg_v->at(i) = _memory->at(*_reg_i + i);
 	}
-	//*_reg_pc += 2;
 }
--- Chip8Emulator/main.cpp
+++ Chip8Emulator/main.cpp
@@ -1,21 +1,34 @@
 #include"Chip8.hpp"
 #include"CRenderer.hpp"
 
+#define OPS 120;
+
+void delayCycle(sf::Clock &c, sf::Time *t);
+
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		std::cout << "Chip8Emulator.exe <ROM>" << std::endl;
 		return 0;
 	}
 
+	// Alloc Mem
+	sf::Clock *clock = new sf::Clock();
+	sf::Time  *time  = new sf::Time;
+
 	Chip8 *chip8 = new Chip8();
 	CRenderer *renderer = new CRenderer();
 
+	// Chip8 initialisieren und Programm laden
 	chip8->init();
 	chip8->loadProg(argv[1]);
 
+	// Display clearen
 	renderer->renderDisplay(chip8->getDisplayMem());
 
 	while (!*chip8->progDone) {
+		// Zeit zurücksetzen
+		*time = time->Zero;
+
 		// Nächsten Opcode ausführen
 		chip8->execute();
 
@@ -25,10 +38,19 @@ int main(int argc, char **argv) {
 
 		// Key-States aktualisieren
 		chip8->handleKeys();
+
+		// Warten bis der Zyklus vorbei ist
+		delayCycle(*clock, time);
 	}
 
 	SAFE_DELETE(chip8);
 	SAFE_DELETE(renderer);
 
 	return 0;
+}
+
+void delayCycle(sf::Clock &c, sf::Time *t) {
+	do {
+		*t += c.restart();
+	} while (t->asMilliseconds() < 1000/60);
 }
